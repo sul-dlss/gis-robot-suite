3752b86da238942f4d2c48f79d41c39b95c6efe6
Darren Hardy <drh@stanford.edu>
2014-07-08 14:27:02 -0700

    added comments

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
+# Ensure subsequent requires search the correct local paths
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "robots"))
 
@@ -11,26 +12,29 @@ ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
 ROBOT_LOG = Logger.new(File.join(ROBOT_ROOT, "log/#{environment}.log"))
 ROBOT_LOG.level = Logger::SEV_LABEL.index(ENV['ROBOT_LOG_LEVEL']) || Logger::INFO
 
-# Override Solrizer's logger before it gets a chance to load and pollute
-# STDERR.
+# Override Solrizer's logger before it gets a chance to load and pollute STDERR.
 begin
   require 'solrizer'
   Solrizer.logger = ROBOT_LOG
 rescue LoadError, NameError, NoMethodError
 end
 
+# Load core robot services
 require 'dor-services'
 require 'lyber_core'
 
 # TODO Maybe move auto-require to just run_robot and spec_helper?
+
+# Load any library files and all the robots
 Dir["#{ROBOT_ROOT}/lib/*.rb"].each { |f| require f }
 require 'robots'
 
+# Load local environment configuration
 env_file = File.expand_path(File.dirname(__FILE__) + "/./environments/#{environment}")
 puts "Loading config from #{env_file}"
 require env_file
 
+# Load Resque configuration and controller
 require 'resque'
 Resque.redis = REDIS_URL || "localhost:6379/resque:#{ENV['ROBOT_ENVIRONMENT']}"
-
 require 'robot-controller'
