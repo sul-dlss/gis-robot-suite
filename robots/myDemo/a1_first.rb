9de507f129b075a0b98984cc395e92babf787aa6
Darren Hardy <drh@stanford.edu>
2014-07-08 13:57:16 -0700

    added comments

--- robots/myDemo/a1_first.rb
+++ robots/myDemo/a1_first.rb
@@ -1,15 +1,20 @@
-module Robots
-  module DorRepo
-    module MyDemo
+# Robot class to run under multiplexing infrastructure
 
-      class A1First
-        include LyberCore::Robot
+module Robots
+  module DorRepo    # Use DorRepo to avoid name collision with Dor module
+    module MyDemo   # This is your workflow package name (using CamelCase)
+
+      class A1First # This is your robot name
+        include LyberCore::Robot # this is the base robot implementation
 
         def initialize
-          super('dor', 'myDemoWF', 'a1-first')
+          super('dor', 'myDemoWF', 'a1-first') # calls LyberCore::Robot initialize
         end
 
+        # perform is the main entry point for the robot
+        # @param [String] druid -- the Druid identifier for the object to process
         def perform(druid)
+          # ... your robot work goes here ...
           LyberCore::Log.info "a1-first worked on #{druid}"
         end
       end
