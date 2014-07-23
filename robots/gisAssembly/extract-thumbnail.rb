403b3034baeb8f08d68a68c10f56b7e8e6c6c61d
Darren Hardy <drh@stanford.edu>
2014-07-23 12:04:53 -0700

    implement initial version

--- robots/gisAssembly/extract-thumbnail.rb
+++ robots/gisAssembly/extract-thumbnail.rb
@@ -18,13 +18,31 @@ module Robots       # Robot package
         # @param [String] druid -- the Druid identifier for the object to process
         def perform(druid)
           LyberCore::Log.debug "extract-thumbnail working on #{druid}"
-          #
-          # ... your robot work goes here ...
-          #
-          # for example:
-          #     obj = Dor::Item.find(druid)
-          #     obj.publish_metadata
-          #
+          extract_thumbnail druid
+        end
+        
+        # Extracts an inline thumbnail from the ESRI ArcCatalog metadata format
+        # @raise [ArgumentError] if cannot find a thumbnail
+        def extract_thumbnail druid
+          rootdir = Dor::Config.geohydra.stage
+          raise ArgumentError, "Missing #{rootdir}" unless File.directory?(rootdir)
+
+          # @param [String] fn the metadata
+          # @param [String] thumbnail_fn the file into which to write JPEG image
+          # @param [String] property_type is the EsriPropertyType to select          
+          fn = Dir.glob("#{rootdir}/#{druid}/temp/*.shp.xml").first
+          thumbnail_fn = File.join(rootdir, druid, 'content', 'preview.jpg')
+          property_type = 'PictureX'
+          
+          doc = Nokogiri::XML(File.read(fn))
+          doc.xpath('//Binary/Thumbnail/Data').each do |node|
+            if property_type.nil? or node['EsriPropertyType'] == property_type
+              image = Base64.decode64(node.text)
+              File.open(thumbnail_fn, 'wb') {|f| f << image }
+              return
+            end
+          end
+          raise ArgumentError, "No thumbnail embedded within #{fn}"
         end
       end
 
