ccfb4d76e7281d1b500447b36ef425c3ab4ab578
Darren Hardy <drh@stanford.edu>
2014-08-20 12:48:15 -0700

    update example with redis format comment

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -1,6 +1,6 @@
 Dor::Config.configure do
 
-  workflow.url 'https://lyberservices-dev.stanford.edu/workflow/'
+  workflow.url 'https://example.com/workflow/'
 
   robots do 
     workspace '/tmp'
@@ -11,9 +11,19 @@ Dor::Config.configure do
   end
 
   dor do
-    service_root 'https://USERNAME:PASSWORD@lyberservices-dev.stanford.edu/dor'
+    service_root 'https://USERNAME:PASSWORD@example.com/dor/v1'
   end
    
 end
 
-REDIS_URL ||= "lyberservices-dev.stanford.edu:6379/resque:#{ENV['ROBOT_ENVIRONMENT']}"
+# @see http://rubydoc.info/gems/redis/3.0.7/file/README.md
+# @see https://github.com/resque/resque
+#
+# Set the redis connection. Takes any of:
+#   String - a redis url string (e.g., 'redis://host:port')
+#   String - 'hostname:port[:db][/namespace]'
+#   Redis - a redis connection that will be namespaced :resque
+#   Redis::Namespace - a namespaced redis connection that will be used as-is
+#   Redis::Distributed - a distributed redis connection that will be used as-is
+#   Hash - a redis connection hash (e.g. {:host => 'localhost', :port => 6379, :db => 0})
+REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db][/namespace]
