808c416c92269518dad601b44692a4d0d85d42a3
Darren Hardy <drh@stanford.edu>
2014-08-20 12:49:13 -0700

    update example with redis format comment

--- config/environments/example.rb
+++ config/environments/example.rb
@@ -35,5 +35,15 @@ Dor::Config.configure do
   end
 end
 
-REDIS_URL ||= "example.com:6379/resque:development"
+# @see http://rubydoc.info/gems/redis/3.0.7/file/README.md
+# @see https://github.com/resque/resque
+#
+# Set the redis connection. Takes any of:
+#   String - a redis url string (e.g., 'redis://host:port')
+#   String - 'hostname:port[:db][/namespace]'
+#   Redis - a redis connection that will be namespaced :resque
+#   Redis::Namespace - a namespaced redis connection that will be used as-is
+#   Redis::Distributed - a distributed redis connection that will be used as-is
+#   Hash - a redis connection hash (e.g. {:host => 'localhost', :port => 6379, :db => 0})
+REDIS_URL = '127.0.0.1:6379/resque:development' # hostname:port[:db][/namespace]
 
