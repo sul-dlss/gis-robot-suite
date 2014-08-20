e050424c5906bf170a82e92379780a9098a03d49
Darren Hardy <drh@stanford.edu>
2014-08-20 12:44:27 -0700

    use robot_environment example

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,3 +5,4 @@ Capistrano::OneTimeKey.generate_one_time_key!
 
 set :deploy_environment, 'development'
 set :whenever_environment, fetch(:deploy_environment)
+set :default_env, { :robot_environment => fetch(:deploy_environment) }
