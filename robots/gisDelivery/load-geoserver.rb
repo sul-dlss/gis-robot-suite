734af18972bbadd6d8a4e82183c42c13200d7b8b
Darren Hardy <drh@stanford.edu>
2014-10-13 15:31:49 -0700

    add fileformat detection

--- robots/gisDelivery/load-geoserver.rb
+++ robots/gisDelivery/load-geoserver.rb
@@ -1,3 +1,6 @@
+ENV['RGEOSERVER_CONFIG'] ||= File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environments', ENV['ROBOT_ENVIRONMENT'] + "_rgeoserver.yml"))
+require 'rgeoserver'
+
 # Robot class to run under multiplexing infrastructure
 module Robots       # Robot package
   module DorRepo    # Use DorRepo/SdrRepo to avoid name collision with Dor module
@@ -20,6 +23,23 @@ module Robots       # Robot package
           LyberCore::Log.debug "load-geoserver working on #{druid}"
           
           rootdir = GisRobotSuite.locate_druid_path druid, type: :stage
+          
+          # determine whether we have a Shapefile/vector or Raster to load
+          modsfn = File.join(rootdir, 'metadata', 'descMetadata.xml')
+          raise RuntimeError, "Cannot locate MODS: #{modsfn}" unless File.exists?(modsfn)
+          format = GisRobotSuite::determine_file_format_from_mods modsfn
+          raise RuntimeError, "Cannot determine file format from MODS: #{modsfn}" if format.nil?
+          
+          # reproject based on file format information
+          mimetype = format.split(/;/).first # nix mimetype flags
+          case mimetype
+          when 'image/tiff'
+            layertype = 'GeoTIFF'
+          when 'application/x-esri-shapefile'
+            layertype = 'PostGIS'
+          else
+            raise RuntimeError, "Unknown format: #{format}"
+          end
 
           raise NotImplementedError # XXX: load to geoserver registry
         end
