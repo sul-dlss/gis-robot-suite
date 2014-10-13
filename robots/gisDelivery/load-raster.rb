3f513cf58909345f5ce3ed67a7f62b8e7343587f
Darren Hardy <drh@stanford.edu>
2014-10-13 14:22:05 -0700

    migrate from GeoHydra

--- robots/gisDelivery/load-raster.rb
+++ robots/gisDelivery/load-raster.rb
@@ -20,11 +20,48 @@ module Robots       # Robot package
           LyberCore::Log.debug "load-raster working on #{druid}"
           
           rootdir = GisRobotSuite.locate_druid_path druid, type: :stage
-                    
-          raise NotImplementedError # XXX: load to GeoTIFF filesystem if raster
+          
+          # determine whether we have a Shapefile to load
+          modsfn = File.join(rootdir, 'metadata', 'descMetadata.xml')
+          raise RuntimeError, "Cannot locate MODS: #{modsfn}" unless File.exists?(modsfn)
+          format = GisRobotSuite::determine_file_format_from_mods modsfn
+          raise RuntimeError, "Cannot determine file format from MODS: #{modsfn}" if format.nil?
+          
+          # reproject based on file format information
+          mimetype = format.split(/;/).first # nix mimetype flags
+          unless mimetype == 'image/tiff'
+            LyberCore::Log.info "#{druid} is not GeoTIFF: #{mimetype}"
+            return
+          end
+          
+          # extract derivative 4326 nomalized content
+          tmpdir = File.join(rootdir, 'temp')
+          zipfn = File.join(rootdir, 'content', 'data_EPSG_4326.zip')
+          raise RuntimeError, "Cannot locate normalized data: #{zipfn}" unless File.exists?(zipfn)
+          tmpdir = extract_data_from_zip druid, zipfn, tmpdir
+          
+          begin
+            Dir.chdir(tmpdir)
+            tiffn = Dir.glob("*.tif").first
+            cmd = "scp #{tiffn} #{Dor::Config.geotiff.hostdir}/#{druid}.tif"
+            LyberCore::Log.debug "Running: #{cmd}"
+            system(cmd)
+          ensure
+            LyberCore::Log.debug "Cleaning: #{tmpdir}"
+            FileUtils.rm_rf tmpdir
+          end
+        end
+        
+        def extract_data_from_zip druid, zipfn, tmpdir
+          LyberCore::Log.debug "Extracting #{druid} data from #{zipfn}"
+          
+          tmpdir = File.join(tmpdir, "loadraster_#{druid}")
+          FileUtils.rm_rf tmpdir if File.directory? tmpdir
+          FileUtils.mkdir_p tmpdir
+          system("unzip '#{zipfn}' -d '#{tmpdir}'")
+          tmpdir
         end
       end
-
     end
   end
 end
--- robots/gisDelivery/load-vector.rb
+++ robots/gisDelivery/load-vector.rb
@@ -12,6 +12,16 @@ module Robots       # Robot package
           super('dor', 'gisDeliveryWF', 'load-vector', check_queued_status: true) # init LyberCore::Robot
         end
 
+        def extract_data_from_zip druid, zipfn, tmpdir
+          LyberCore::Log.debug "Extracting #{druid} data from #{zipfn}"
+          
+          tmpdir = File.join(tmpdir, "loadvector_#{druid}")
+          FileUtils.rm_rf tmpdir if File.directory? tmpdir
+          FileUtils.mkdir_p tmpdir
+          system("unzip '#{zipfn}' -d '#{tmpdir}'")
+          tmpdir
+        end
+
         # `perform` is the main entry point for the robot. This is where
         # all of the robot's work is done.
         #
@@ -20,8 +30,56 @@ module Robots       # Robot package
           LyberCore::Log.debug "load-vector working on #{druid}"
 
           rootdir = GisRobotSuite.locate_druid_path druid, type: :stage
+          
+          # determine whether we have a Shapefile to load
+          modsfn = File.join(rootdir, 'metadata', 'descMetadata.xml')
+          raise RuntimeError, "Cannot locate MODS: #{modsfn}" unless File.exists?(modsfn)
+          format = GisRobotSuite::determine_file_format_from_mods modsfn
+          raise RuntimeError, "Cannot determine file format from MODS: #{modsfn}" if format.nil?
+          
+          # reproject based on file format information
+          mimetype = format.split(/;/).first # nix mimetype flags
+          unless mimetype == 'application/x-esri-shapefile'
+            LyberCore::Log.info "#{druid} is not Shapefile: #{mimetype}"
+            return
+          end
+          
+          # extract derivative 4326 nomalized content
+          tmpdir = File.join(rootdir, 'temp')
+          zipfn = File.join(rootdir, 'content', 'data_EPSG_4326.zip')
+          raise RuntimeError, "Cannot locate normalized data: #{zipfn}" unless File.exists?(zipfn)
+          tmpdir = extract_data_from_zip druid, zipfn, tmpdir
+          
+          begin
+            schema = Dor::Config.postgis.schema || 'druid'
+            encoding = 'UTF-8'
+            projection = '4326' # always use EPSG:4326 derivative
+            
+            # sniff out shapefile from extraction
+            Dir.chdir(tmpdir)
+            shpfn = Dir.glob("*.shp").first
+            sqlfn = shpfn.gsub(/\.shp$/, '.sql')
+            LyberCore::Log.debug "Working on Shapefile #{shpfn}"
 
-          raise NotImplementedError # XXX: load to PostGIS database if shapefile
+            # XXX: Perhaps put the .sql data into the content directory as .zip for derivative
+            # XXX: -G for the geography column causes some issues with GeoServer
+            cmd = "shp2pgsql -s #{projection} -d -D -I -W #{encoding}" +
+                   " '#{shpfn}' #{schema}.#{druid} " +
+                   "> '#{sqlfn}'"
+            LyberCore::Log.debug "Running: #{cmd}"
+            system(cmd)
+            
+            if File.exists?(sqlfn)
+              # XXX: Assumes PGHOST, PGUSER, PGDATABASE are set in environment
+              cmd = 'psql --no-psqlrc --no-password --quiet ' +
+                     "--file='#{sqlfn}' "
+              LyberCore::Log.debug "Running: #{cmd}"
+              system(cmd)
+            end 
+          ensure
+            LyberCore::Log.debug "Cleaning: #{tmpdir}"
+            FileUtils.rm_rf tmpdir
+          end          
         end
       end
 
