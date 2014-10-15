cc5981c8b369446def3352b42d411ed6fda46dc6
Darren Hardy <drh@stanford.edu>
2014-10-15 14:23:32 -0700

    add example REST API call for seeding geowebcache

--- robots/gisDelivery/seed-geowebcache.rb
+++ robots/gisDelivery/seed-geowebcache.rb
@@ -19,6 +19,45 @@ module Robots       # Robot package
         def perform(druid)
           LyberCore::Log.debug "seed-geowebcache working on #{druid}"
           
+          # Remote Address:171.67.35.173:80
+          # Request URL:http://kurma-podd1.stanford.edu/geoserver/gwc/rest/seed/druid:bq621wf4873
+          # Request Method:POST
+          # Status Code:200 OK
+          # Request Headersview source
+          # Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+          # Accept-Encoding:gzip,deflate
+          # Accept-Language:en-US,en;q=0.8
+          # Cache-Control:max-age=0
+          # Connection:keep-alive
+          # Content-Length:152
+          # Content-Type:application/x-www-form-urlencoded
+          # Cookie:JSESSIONID=514C01AF5A2AF088D8224A3059379581; _ga=GA1.2.667756165.1404843603
+          # Host:kurma-podd1.stanford.edu
+          # Origin:http://kurma-podd1.stanford.edu
+          # Referer:http://kurma-podd1.stanford.edu/geoserver/gwc/rest/seed/druid:bq621wf4873
+          # User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36
+          # Form Dataview sourceview URL encoded
+          # threadCount:01
+          # type:seed
+          # gridSetId:EPSG:900913
+          # format:image/png
+          # zoomStart:00
+          # zoomStop:10
+          # parameter_STYLES:druid_bq621wf48733
+          # minX:
+          # minY:
+          # maxX:
+          # maxY:
+          # Response Headersview source
+          # Connection:Keep-Alive
+          # Content-Encoding:gzip
+          # Content-Length:981
+          # Content-Type:text/html;charset=ISO-8859-1
+          # Date:Wed, 15 Oct 2014 20:38:33 GMT
+          # Keep-Alive:timeout=5, max=100
+          # Server:Noelios-Restlet-Engine/1.0..8
+          
+          
           raise NotImplementedError # XXX: seed tiles using geoserver's built-in geowebcache
         end
       end
