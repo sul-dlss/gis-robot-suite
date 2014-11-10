c2524c5c948a10cfa816016643cba88334d4d59f
Darren Hardy <drh@stanford.edu>
2014-11-10 13:42:07 -0800

    do assembly not accessioning

--- robots/gisAssembly/start-assembly-workflow.rb
+++ robots/gisAssembly/start-assembly-workflow.rb
@@ -3,13 +3,13 @@ module Robots       # Robot package
   module DorRepo    # Use DorRepo/SdrRepo to avoid name collision with Dor module
     module GisAssembly   # This is your workflow package name (using CamelCase)
 
-      class StartAccessioningWorkflow # This is your robot name (using CamelCase)
+      class StartAssemblyWorkflow # This is your robot name (using CamelCase)
         # Build off the base robot implementation which implements
         # features common to all robots
         include LyberCore::Robot 
 
         def initialize
-          super('dor', 'gisAssemblyWF', 'start-accessioning-workflow', check_queued_status: true) # init LyberCore::Robot
+          super('dor', 'gisAssemblyWF', 'start-assembly-workflow', check_queued_status: true) # init LyberCore::Robot
         end
 
         # `perform` is the main entry point for the robot. This is where
@@ -17,10 +17,10 @@ module Robots       # Robot package
         #
         # @param [String] druid -- the Druid identifier for the object to process
         def perform(druid)
-          LyberCore::Log.debug "start-accessioning-workflow working on #{druid}"
+          LyberCore::Log.debug "start-assembly-workflow working on #{druid}"
 
           i = Dor::Item.find(druid)
-          i.initialize_workflow 'accessioningWF', 'dor'
+          i.initialize_workflow 'assemblyWF', 'dor'
         end
       end
 
