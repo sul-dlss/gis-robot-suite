d159ae1c57bb1e1f9f19fb11e523df70de8a5158
Darren Hardy <drh@stanford.edu>
2014-11-11 16:34:33 -0800

    add kurma-robots1-stage

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,8 @@
-server 'geomdtk-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+# server 'geomdtk-test.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
+server 'kurma-robots1-stage.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
-set :deploy_environment, 'test'
+set :deploy_environment, 'staging'
 set :whenever_environment, fetch(:deploy_environment)
 set :default_env, { :robot_environment => fetch(:deploy_environment) }
