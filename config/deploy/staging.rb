836811bd541520fffd6734fd8307ab6291b54d80
Darren Hardy <drh@stanford.edu>
2015-07-07 10:13:49 -0700

    fix stage vs staging in deploy for robot_environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ server 'kurma-robots1-stage.stanford.edu', user: 'lyberadmin', roles: %w(web app
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :deploy_environment, 'staging'
-set :whenever_environment, fetch(:deploy_environment)
-set :default_env, robot_environment: fetch(:deploy_environment)
+set :whenever_environment, 'stage'
+set :default_env, robot_environment: 'stage'
