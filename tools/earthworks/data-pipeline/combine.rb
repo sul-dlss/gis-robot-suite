0d7208e508e60074f193848ccf15f1f614cf389e
Darren Hardy <drh@stanford.edu>
2016-08-02 12:43:58 -0700

    add curate.rb to pipeline to remove layers we do not want

--- tools/earthworks/data-pipeline/combine.rb
+++ tools/earthworks/data-pipeline/combine.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 require 'json'
 
-SKIP = %w(princeton-th83m123j sde-columbia-sudanadmn4)
-
 docs = []
 ARGV.each do |dir|
   Dir.glob("#{dir}/**/{filtered,geoblacklight}*.json") do |fn|
@@ -10,8 +8,7 @@ ARGV.each do |dir|
     data = JSON.parse(File.read(fn))
     data = [data] unless data.is_a? Array
     data.each do |doc|
-      next if doc.nil? || SKIP.include?(doc['layer_slug_s']) || doc['dct_provenance_s'] == ''
-      docs << doc
+      docs << doc unless doc.nil?
     end
   end
 end
---
+++ tools/earthworks/data-pipeline/curate.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'json'
+
+SKIP_LAYERS = %w(
+  princeton-th83m123j
+  sde-columbia-sudanadmn4
+)
+
+docs = JSON.parse(File.read('geoblacklight.json'))
+docs = [docs] unless docs.is_a? Array
+docs.delete_if do |doc|
+  doc.nil? || SKIP_LAYERS.include?(doc['layer_slug_s']) || doc['dct_provenance_s'] == ''
+end
+
+puts "Saving to geoblacklight.json"
+File.open('geoblacklight.json', 'w') {|f| f << JSON.pretty_generate(docs)}
