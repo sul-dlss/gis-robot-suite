fa3843bf5a1f435e045382dc7f7447e15003f4e1
Darren Hardy <drh@stanford.edu>
2016-08-04 12:52:27 -0700

    delete all Image types

--- tools/earthworks/data-pipeline/curate.rb
+++ tools/earthworks/data-pipeline/curate.rb
@@ -5,10 +5,17 @@ SKIP_LAYERS = %w(
   sde-columbia-sudanadmn4
 )
 
+SKIP_TYPES = %w(
+  Image
+)
+
 docs = JSON.parse(File.read('geoblacklight.json'))
 docs = [docs] unless docs.is_a? Array
 docs.delete_if do |doc|
-  doc.nil? || SKIP_LAYERS.include?(doc['layer_slug_s']) || doc['dct_provenance_s'] == ''
+  doc.nil? ||
+  SKIP_LAYERS.include?(doc['layer_slug_s']) ||
+  SKIP_TYPES.include?(doc['layer_geom_type_s']) || 
+  doc['dct_provenance_s'] == ''
 end
 
 puts "Saving to geoblacklight.json"
