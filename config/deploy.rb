22f8dea266260b090f719bb343f85821c89ba100
Naomi Dushay <ndushay@stanford.edu>
2017-06-19 13:29:48 -0700

    capistrano updates to current best practices

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid only for Capistrano 3.1
-# lock '3.2.1'
-
 set :rvm_type, :system
 set :rvm_ruby_string, 'ruby-2.2.2'
 
@@ -16,7 +13,7 @@ set :deploy_to, '/opt/app/lyberadmin/gis-robot-suite'
 
 # Default value for :scm is :git
 # set :scm, :git
-set :scm, :git
+
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -38,8 +35,6 @@ set :scm, :git
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :stages, %w(development staging production)
-set :default_stage, 'development'
 set :linked_dirs, %w(log run config/environments config/certs config/ArcGIS)
 
 namespace :deploy do
