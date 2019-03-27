43c1e65a4d1255f9262a2a8d3fbf80c1ab6a42d9
Peter Mangiafico <peter@mangiafico.org>
2019-03-27 16:26:26 -0700

    remove dev deploy environment that is gone

--- config/deploy/dev.rb
+++
@@ -1,7 +0,0 @@
-server 'kurma-robots1-dev.stanford.edu', user: 'lyberadmin', roles: %w(web app db)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :deploy_environment, 'development'
-set :whenever_environment, fetch(:deploy_environment)
-set :default_env, robot_environment: fetch(:deploy_environment)
