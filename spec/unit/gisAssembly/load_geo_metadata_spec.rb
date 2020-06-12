bdeec58136a09bc8b1087e89d96b41dcfd2f057e
Justin Coyne <jcoyne@justincoyne.com>
2020-06-12 11:06:30 -0500

    Update geoMetadata using the dor-services-app legacy metadata endpoint

--- spec/unit/gisAssembly/load_geo_metadata_spec.rb
+++ spec/unit/gisAssembly/load_geo_metadata_spec.rb
@@ -14,12 +14,11 @@ RSpec.describe Robots::DorRepo::GisAssembly::LoadGeoMetadata do
     let(:fake_tags_client) do
       instance_double(Dor::Services::Client::AdministrativeTags, list: [], create: nil)
     end
-    let(:geo_metadata) { instance_double(Dor::GeoMetadataDS, :content= => true) }
-    let(:item) do
-      instance_double(Dor::Item,
-                      datastreams: { 'geoMetadata' => geo_metadata },
-                      save: true,
-                      pid: 'druid:bc123df4567')
+    let(:object_client) do
+      instance_double(Dor::Services::Client::Object, metadata: metadata_client)
+    end
+    let(:metadata_client) do
+      instance_double(Dor::Services::Client::Metadata, legacy_update: true)
     end
     let(:xml) do
       <<~XML
@@ -30,15 +29,21 @@ RSpec.describe Robots::DorRepo::GisAssembly::LoadGeoMetadata do
     before do
       allow(Dor::Config).to receive(:geohydra).and_return(stub_config)
       allow(File).to receive(:size?).and_return(100)
-      allow(Dor::Item).to receive(:find).and_return(item)
+      allow(Dor::Services::Client).to receive(:object).and_return(object_client)
       allow(File).to receive(:read).and_return(xml)
-      allow(robot).to receive(:tags_client).with(item.pid).and_return(fake_tags_client)
+      allow(File).to receive(:mtime).and_return(Time.now)
+      allow(robot).to receive(:tags_client).with(druid).and_return(fake_tags_client)
     end
 
     it 'tags the object' do
       robot.perform(druid)
       expect(fake_tags_client).to have_received(:create).once.with(tags: ['Dataset : GIS'])
-      expect(item).to have_received(:save)
+      expect(metadata_client).to have_received(:legacy_update).with(
+        geo: {
+          updated: Time,
+          content: "<xml />\n"
+        }
+      )
     end
   end
 end
