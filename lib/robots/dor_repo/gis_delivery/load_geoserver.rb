6e8620c76c2cd82de707c7a088cecd5ed2d9af28
Jack Reed <phillipjreed@gmail.com>
2020-07-06 14:51:05 -0600

    Remove RGeoServer for load-raster stuff

--- lib/robots/dor_repo/gis_delivery/load_geoserver.rb
+++ lib/robots/dor_repo/gis_delivery/load_geoserver.rb
@@ -67,7 +67,7 @@ module Robots       # Robot package
           if layer['vector'] && layer['vector']['format'] == 'PostGIS'
             create_vector(connection, ws, layer['vector'], workspace_name)
           elsif layer['raster'] && layer['raster']['format'] == 'GeoTIFF'
-            create_raster(catalog, ws, layer['raster'])
+            create_raster(connection, ws, layer['raster'], workspace_name)
           else
             fail "load-geoserver: #{druid} has unknown layer format: #{layer}"
           end
@@ -140,51 +140,72 @@ module Robots       # Robot package
           end
         end
 
-        def create_raster(catalog, ws, layer)
+        def create_raster(connection, ws, layer, workspace_name)
           druid = layer['druid']
           %w(title abstract keywords).each do |i|
             fail ArgumentError, "load-geoserver: #{druid}: Layer is missing #{i}" unless layer.include?(i) && !layer[i].empty?
           end
 
           # create coverage store
-          LyberCore::Log.debug "Retrieving CoverageStore: #{ws.name}/#{druid}"
-          cs = RGeoServer::CoverageStore.new catalog, workspace: ws, name: druid
-          if cs.new?
-            LyberCore::Log.debug "Creating CoverageStore: #{ws.name}/#{cs.name}"
-            cs.enabled = true
-            cs.description = layer['title']
-            cs.data_type = 'GeoTIFF'
-            cs.url = "file:#{Settings.geohydra.geotiff.dir}/#{druid}.tif"
+          LyberCore::Log.debug "Retrieving CoverageStore: #{workspace_name}/#{druid}"
+          coverage_store = Geoserver::Publish::CoverageStore.new(connection)
+          coverage_store_exists = coverage_store.find(
+            coverage_store_name: druid,
+            workspace_name: workspace_name
+          )
+          if coverage_store_exists.nil?
+            LyberCore::Log.debug "Creating CoverageStore: #{workspace_name}/#{druid}"
             begin
-              cs.save
-            rescue RGeoServer::GeoServerInvalidRequest => e
+              coverage_store.create(
+                workspace_name: workspace_name,
+                coverage_store_name: druid,
+                url: "file:#{Settings.geohydra.geotiff.dir}/#{druid}.tif",
+                type: 'GeoTIFF',
+                additional_payload: {
+                  description: layer['title']
+                }
+              )
+            rescue Geoserver::Publish::Error => e
               fail "load-geoserver: #{druid} cannot save CoverageStore: #{e.message}"
             end
           else
-            LyberCore::Log.debug "load-geoserver:: #{druid} found existing CoverageStore: #{ws.name}/#{cs.name}"
-            fail "load-geoserver: #{druid} found disabled CoverageStore" unless cs.enabled
+            LyberCore::Log.debug "load-geoserver:: #{druid} found existing CoverageStore: #{workspace_name}/#{druid}"
           end
 
           # create or update coverage
-          LyberCore::Log.debug "Retrieving Coverage: #{ws.name}/#{cs.name}/#{druid}"
-          cv = RGeoServer::Coverage.new catalog, workspace: ws, coverage_store: cs, name: druid
-          if cv.new?
+          LyberCore::Log.debug "Retrieving Coverage: #{workspace_name}/#{druid}/#{druid}"
+          coverage = Geoserver::Publish::Coverage.new(connection)
+          coverage_exists = coverage.find(
+            coverage_name: druid,
+            coverage_store_name: druid,
+            workspace_name: workspace_name
+          )
+
+          if coverage_exists.nil?
             LyberCore::Log.debug "Creating Coverage #{druid}"
           else
             LyberCore::Log.debug "Found existing Coverage #{druid}"
           end
+          coverage_struct = Struct.new(:enabled, :title, :abstract, :keywords, :metadata_links, :metadata)
+          cv = coverage_struct.new
           cv.enabled = true
           cv.title = layer['title']
           cv.abstract = layer['abstract']
           cv.keywords = [cv.keywords, layer['keywords']].flatten.compact.uniq
           cv.metadata_links = []
-          cv.metadata = cv.metadata.merge!(
+          cv.metadata = cv.metadata || {}.merge!(
             'cacheAgeMax' => 86400,
             'cachingEnabled' => true
           )
           begin
-            cv.save
-          rescue RGeoServer::GeoServerInvalidRequest => e
+            coverage.create(
+              workspace_name: workspace_name,
+              coverage_store_name: druid,
+              coverage_name: druid,
+              title: layer['title'],
+              additional_payload: cv.to_h
+            )
+          rescue Geoserver::Publish::Error => e
             fail "load-geoserver: #{druid} cannot save Coverage: #{e.message}"
           end
 
@@ -196,7 +217,7 @@ module Robots       # Robot package
             raster_style = 'raster'
           end
           LyberCore::Log.debug "load-geoserver: #{druid} determined raster style as '#{raster_style}'"
-
+          style = Geoserver::Publish::Style.new(connection)
           # need to create a style if it's a min/max style
           if raster_style =~ /^raster_grayscale_(.+)_(.+)$/
             _min = Regexp.last_match(1).to_f.floor
@@ -230,35 +251,42 @@ module Robots       # Robot package
               puts sldtxt
 
               # create a style with the SLD definition
-              style = RGeoServer::Style.new catalog, name: raster_style
-              LyberCore::Log.debug "load-geoserver: #{druid} loaded style #{style.name}"
-              if style.new?
-                style.sld_doc = sldtxt
-                LyberCore::Log.debug "load-geoserver: #{druid} saving new style #{style.name}"
-                style.save
+              style_exists = style.find(
+                style_name: raster_style
+              )
+              LyberCore::Log.debug "load-geoserver: #{druid} loaded style #{raster_style}"
+              if style_exists.nil?
+                LyberCore::Log.debug "load-geoserver: #{druid} saving new style #{raster_style}"
+                style.create(style_name: raster_style)
+                style.update(style_name: raster_style, filename: nil, payload: sldtxt)
               end
             else
               raster_style = 'raster_grayband' # a simple band-oriented histogram adjusted style
-              style = RGeoServer::Style.new catalog, name: raster_style
-              fail "load-geoserver: #{druid} has missing style #{raster_style} on #{catalog}" if style.new?
+              style_exists = style.find(
+                style_name: raster_style
+              )
+              fail "load-geoserver: #{druid} has missing style #{raster_style}" if style_exists.nil?
             end
           else
-            style = RGeoServer::Style.new catalog, name: raster_style
-            fail "load-geoserver: #{druid} has missing style #{raster_style} on #{catalog}" if style.new?
+            style_exists = style.find(
+              style_name: raster_style
+            )
+            fail "load-geoserver: #{druid} has missing style #{raster_style}" if style_exists.nil?
           end
 
           # fetch layer to load raster style - it's created when the coverage is created via REST API
-          lyr = RGeoServer::Layer.new catalog, workspace: ws, name: druid
-          if lyr.new?
-            fail "load-geoserver: Layer #{druid} is missing for coverage #{ws.name}/#{cs.name}/#{druid}"
+          layer = Geoserver::Publish::Layer.new(connection)
+          layer_exists = layer.find(layer_name: druid)
+          if layer_exists.nil?
+            fail "load-geoserver: Layer #{druid} is missing for coverage #{workspace_name}/#{druid}/#{druid}"
           end
 
-          if lyr.default_style != style.name
-            lyr.default_style = style.name
-            LyberCore::Log.debug "load-geoserver: #{druid} updating #{lyr.name} with default style #{style.name}"
+          if layer_exists.dig('layer', 'defaultStyle', 'name') != raster_style
+            layer_exists['layer']['defaultStyle'] = raster_style
+            LyberCore::Log.debug "load-geoserver: #{druid} updating #{druid} with default style #{raster_style}"
             begin
-              lyr.save
-            rescue RGeoServer::GeoServerInvalidRequest => e
+              layer.update(layer_name: druid, additional_payload: layer_exists)
+            rescue Geoserver::Publish::Error => e
               fail "load-geoserver: #{druid} cannot save Layer: #{e.message}"
             end
           end
--- spec/robots/dor_repo/gis_delivery/load_geoserver_spec.rb
+++ spec/robots/dor_repo/gis_delivery/load_geoserver_spec.rb
@@ -48,45 +48,35 @@ RSpec.describe Robots::DorRepo::GisDelivery::LoadGeoserver do
     describe 'loading a raster dataset' do
       let(:druid) { 'dg548ft1892' }
       let(:store_post_body) do
-        "<?xml version=\"1.0\"?>\n<coverageStore>\n  <name>dg548ft1892</name>\n  <workspace>\n    <name>druid</name>\n  </workspace>\n  <enabled>true</enabled>\n  <type>GeoTIFF</type>\n  <description>1000 Meter Resolution Bathymetry Grid of Exclusive Economic Zone (EEZ): Russian River Basin, California, 1998</description>\n  <url>file:/geotiff/dg548ft1892.tif</url>\n</coverageStore>\n" # rubocop:disable Layout/LineLength
+        "{\"coverageStore\":{\"name\":\"dg548ft1892\",\"url\":\"file:/geotiff/dg548ft1892.tif\",\"enabled\":true,\"workspace\":{\"name\":\"druid\"},\"type\":\"GeoTIFF\",\"_default\":false,\"description\":\"1000 Meter Resolution Bathymetry Grid of Exclusive Economic Zone (EEZ): Russian River Basin, California, 1998\"}}" # rubocop:disable Layout/LineLength
       end
       let(:coverage_post_body) do
-        "<?xml version=\"1.0\"?>\n<coverage>\n  <nativeName>dg548ft1892</nativeName>\n  <name>dg548ft1892</name>\n  <title>1000 Meter Resolution Bathymetry Grid of Exclusive Economic Zone (EEZ): Russian River Basin, California, 1998</title>\n  <abstract>Eez1000 is a 1000 meter resolution statewide bathymetric dataset that generally covers the Exclusive Economic Zone (EEZ), an area extending 200 nautical miles from all United States possessions and trust territories. The data was adapted from isobath values ranging from 200 meters to 4800 meters below sea level; therefore nearshore depictions ARE NOT ACCURATE and \"flatten out\" between 200 meter depths and the coastline. The data is intended only for general portrayals of offshore features and depths. The Department of Fish and Game (DFG), Technical Services Branch (TSB) GIS Unit received the source data in the form of a line contour coverage (known as DFG's eezbath) from the United States Geological Survey (USGS). The contour data was converted to a TIN (triangulated irregular network) using ArcView 3D Analyst and then converted to a grid. The contour data was previously reprojected by TSB to Albers conic equal-area using standard Teale Data Center parameters. Some minor aesthetic editing was performed on peripheral areas using the ARC/INFO Grid EXPAND function. The image version was created using the ARC/INFO GRIDIMAGE function. Please see the attached metadata file \"eezbatcall.doc\" or the DFG coverage metadata \"eezbath.txt\" for further source data information.\nThis layer can be used for watershed analysis and planning in the Russian River region of California.</abstract>\n  <enabled>true</enabled>\n  <keywords>\n    <keyword>Hydrography</keyword>\n    <keyword>Watersheds</keyword>\n    <keyword>Inland Waters</keyword>\n    <keyword>Sonoma County (Calif.)</keyword>\n    <keyword>Mendocino County (Calif.)</keyword>\n    <keyword>Russian River Watershed (Calif.)</keyword>\n  </keywords>\n</coverage>\n" # rubocop:disable Layout/LineLength
+        "{\"coverage\":{\"enabled\":true,\"name\":\"dg548ft1892\",\"title\":\"1000 Meter Resolution Bathymetry Grid of Exclusive Economic Zone (EEZ): Russian River Basin, California, 1998\",\"abstract\":\"Eez1000 is a 1000 meter resolution statewide bathymetric dataset that generally covers the Exclusive Economic Zone (EEZ), an area extending 200 nautical miles from all United States possessions and trust territories. The data was adapted from isobath values ranging from 200 meters to 4800 meters below sea level; therefore nearshore depictions ARE NOT ACCURATE and \\\"flatten out\\\" between 200 meter depths and the coastline. The data is intended only for general portrayals of offshore features and depths. The Department of Fish and Game (DFG), Technical Services Branch (TSB) GIS Unit received the source data in the form of a line contour coverage (known as DFG's eezbath) from the United States Geological Survey (USGS). The contour data was converted to a TIN (triangulated irregular network) using ArcView 3D Analyst and then converted to a grid. The contour data was previously reprojected by TSB to Albers conic equal-area using standard Teale Data Center parameters. Some minor aesthetic editing was performed on peripheral areas using the ARC/INFO Grid EXPAND function. The image version was created using the ARC/INFO GRIDIMAGE function. Please see the attached metadata file \\\"eezbatcall.doc\\\" or the DFG coverage metadata \\\"eezbath.txt\\\" for further source data information.\\nThis layer can be used for watershed analysis and planning in the Russian River region of California.\",\"keywords\":[\"Hydrography\",\"Watersheds\",\"Inland Waters\",\"Sonoma County (Calif.)\",\"Mendocino County (Calif.)\",\"Russian River Watershed (Calif.)\"],\"metadata_links\":[],\"metadata\":{\"cacheAgeMax\":86400,\"cachingEnabled\":true}}}" # rubocop:disable Layout/LineLength
       end
       let(:layer_put_body) do
-        "<?xml version=\"1.0\"?>\n<layer>\n  <name>dg548ft1892</name>\n  <path/>\n  <type/>\n  <enabled/>\n  <queryable/>\n  <defaultStyle>\n    <name>raster</name>\n  </defaultStyle>\n  <metadata/>\n  <attribution>\n    <logoWidth/>\n    <logoHeight/>\n  </attribution>\n</layer>\n" # rubocop:disable Layout/LineLength
+        "{\"layer\":{\"name\":\"dg548ft1892\",\"path\":\"\",\"type\":\"RASTER\",\"defaultStyle\":\"raster\",\"resource\":{\"@class\":\"coverage\",\"name\":\"druid:dg548ft1892\",\"href\":\"http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages/dg548ft1892.json\"},\"queryable\":false,\"opaque\":false}}" # rubocop:disable Layout/LineLength
       end
 
       before do
-        stub_request(:get, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892.xml')
+        stub_request(:get, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892')
           .to_return(status: 404)
-        stub_request(:get, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages/dg548ft1892.xml')
+        stub_request(:get, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages/dg548ft1892')
           .to_return(status: 404)
-        stub_request(:get, "http://example.com/geoserver/rest/workspaces/druid/datastores.xml")
-          .to_return(status: 200, body: read_fixture('geoserver_responses/datastores.xml'), headers: {})
-        stub_request(:get, "http://example.com/geoserver/rest/workspaces/druid/coveragestores.xml")
-          .to_return(status: 200, body: '<coverageStores/>', headers: {})
-        stub_request(:get, "http://example.com/geoserver/rest/workspaces/druid/wmsstores.xml")
-          .to_return(status: 200, body: '<wmsStores/>', headers: {})
-        stub_request(:get, "http://example.com/geoserver/rest/workspaces/druid/wmtsstores.xml")
-          .to_return(status: 200, body: '<wmtsStores/>', headers: {})
-        stub_request(:get, 'http://example.com/geoserver/rest/styles/raster.xml')
-          .to_return(status: 200, body: '<style><name>raster</name><format>sld</format><languageVersion><version>1.0.0</version></languageVersion><filename>raster.sld</filename></style>') # rubocop:disable Layout/LineLength
-        stub_request(:get, 'http://example.com/geoserver/rest/styles/raster.sld')
-          .to_return(status: 200, body: '<StyledLayerDescriptor />')
-        stub_request(:get, 'http://example.com/geoserver/rest/layers/dg548ft1892.xml')
-          .to_return(status: 200, body: '<layer><defaultStyle><name>not-raster</name></defaultStyle><resource class="coverage"><name>druid:dg548ft1892</name><atom:link xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="https://kurma-geoserver1-stage.stanford.edu/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages/dg548ft1892.xml" type="application/xml"/></resource></layer>') # rubocop:disable Layout/LineLength
+        stub_request(:get, 'http://example.com/geoserver/rest/styles/raster')
+          .to_return(status: 200, body: '{"style":{"name":"raster","format":"sld","languageVersion":{"version":"1.0.0"},"filename":"raster.sld"}}')
+        stub_request(:get, 'http://example.com/geoserver/rest/layers/dg548ft1892')
+          .to_return(status: 200, body: '{"layer":{"name":"dg548ft1892","path":"","type":"RASTER","defaultStyle":{"name":"raster_rgb8","href":"http://example.com/geoserver/rest/styles/raster_rgb8.json"},"resource":{"@class":"coverage","name":"druid:dg548ft1892","href":"http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages/dg548ft1892.json"},"queryable":false,"opaque":false}}') # rubocop:disable Layout/LineLength
       end
 
       it 'runs without error' do
-        stubbed_store_post = stub_request(:post, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores.xml')
-                             .with(headers: { 'Content-Type' => 'application/xml' }, body: store_post_body)
+        stubbed_store_post = stub_request(:post, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores')
+                             .with(headers: { 'Content-Type' => 'application/json' }, body: store_post_body)
                              .to_return(status: 201)
-        stubbed_coverage_post = stub_request(:post, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages.xml')
-                                .with(headers: { 'Content-Type' => 'application/xml' }, body: coverage_post_body)
+        stubbed_coverage_post = stub_request(:post, 'http://example.com/geoserver/rest/workspaces/druid/coveragestores/dg548ft1892/coverages')
+                                .with(headers: { 'Content-Type' => 'application/json' }, body: coverage_post_body)
                                 .to_return(status: 201)
-        stubbed_layer_put = stub_request(:put, 'http://example.com/geoserver/rest/layers/dg548ft1892.xml?layer=dg548ft1892')
-                            .with(headers: { 'Content-Type' => 'application/xml' }, body: layer_put_body)
+        stubbed_layer_put = stub_request(:put, 'http://example.com/geoserver/rest/layers/dg548ft1892')
+                            .with(headers: { 'Content-Type' => 'application/json' }, body: layer_put_body)
                             .to_return(status: 201)
         robot.perform(druid)
         expect(stubbed_store_post).to have_been_requested
