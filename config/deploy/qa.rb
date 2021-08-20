755b7eb037d40d92443189bd73d00ef02b127426
Johnathan Martin <john.martin@stanford.edu>
2021-08-20 16:36:39 -0700

    QA should see itself as production

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -4,5 +4,5 @@ server 'kurma-robots1-qa.stanford.edu', user: 'lyberadmin', roles: %w[web app db
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
-set :deploy_environment, 'qa'
+set :deploy_environment, 'production'
 set :default_env, robot_environment: fetch(:deploy_environment)
