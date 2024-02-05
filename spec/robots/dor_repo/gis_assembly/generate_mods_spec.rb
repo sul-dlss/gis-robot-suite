8c9d5f0830ee41e2c5377c1072b282bfad306f5f
Naomi Dushay <ndushay@stanford.edu>
2024-02-05 12:37:49 -0800

    address rubocop RSpec/NamedSubject

--- spec/robots/dor_repo/gis_assembly/generate_mods_spec.rb
+++ spec/robots/dor_repo/gis_assembly/generate_mods_spec.rb
@@ -184,19 +184,19 @@ RSpec.describe Robots::DorRepo::GisAssembly::GenerateMods do
 
   describe '.dd2ddmmss_abs' do
     it 'converts DD to DDMMSS' do
-      expect(subject.send(:dd2ddmmss_abs, '-109.758319')).to eq('109°45ʹ30ʺ')
-      expect(subject.send(:dd2ddmmss_abs, '48.999336')).to eq('48°59ʹ58ʺ')
+      expect(described_class.new.send(:dd2ddmmss_abs, '-109.758319')).to eq('109°45ʹ30ʺ')
+      expect(described_class.new.send(:dd2ddmmss_abs, '48.999336')).to eq('48°59ʹ58ʺ')
     end
   end
 
   describe '.to_coordinates_ddmmss' do
     it 'converts MARC to DDMMSS' do
-      expect(subject.send(:to_coordinates_ddmmss, '-180 -- 180/90 -- -90')).to eq('W 180°--E 180°/N 90°--S 90°')
-      expect(subject.send(:to_coordinates_ddmmss, '-109.758319 -- -88.990844/48.999336 -- 29.423028')).to eq('W 109°45ʹ30ʺ--W 88°59ʹ27ʺ/N 48°59ʹ58ʺ--N 29°25ʹ23ʺ')
+      expect(described_class.new.send(:to_coordinates_ddmmss, '-180 -- 180/90 -- -90')).to eq('W 180°--E 180°/N 90°--S 90°')
+      expect(described_class.new.send(:to_coordinates_ddmmss, '-109.758319 -- -88.990844/48.999336 -- 29.423028')).to eq('W 109°45ʹ30ʺ--W 88°59ʹ27ʺ/N 48°59ʹ58ʺ--N 29°25ʹ23ʺ')
     end
 
     it 'handles bad arguments' do
-      expect { subject.send(:to_coordinates_ddmmss, '-185 -- 185/95 -- -95') }.to raise_error(ArgumentError)
+      expect { described_class.new.send(:to_coordinates_ddmmss, '-185 -- 185/95 -- -95') }.to raise_error(ArgumentError)
     end
   end
 end
