621854d86df5909d0d98bce8345f4b25828ca8e7
Michael J. Giarlo <mjgiarlo@stanford.edu>
2024-04-08 16:55:58 -0700

    Add robot to start GIS assembly workflow
    Connects to sul-dlss/workflow-server-rails#750

---
+++ lib/robots/dor_repo/gis_assembly/start_gis_assembly_workflow.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Robots
+  module DorRepo
+    module GisAssembly
+      # Kicks off GIS assembly by making sure the item is open
+      class StartGisAssemblyWorkflow < Base
+        def initialize
+          super('gisAssemblyWF', 'start-gis-assembly-workflow')
+        end
+
+        def perform_work
+          Honeybadger.notify('[WARNING] GIS assembly has been started with an object that is not open') unless object_client.version.status.open?
+        end
+      end
+    end
+  end
+end
