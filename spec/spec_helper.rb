ea9f817c007cb2db68da08c49754ca1b9b7ca76a
Justin Coyne <jcoyne@justincoyne.com>
2026-06-10 14:36:34 -0500

    Remove unused test method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,15 +22,10 @@ require File.expand_path("#{__dir__}/../config/boot")
 require 'debug'
 require 'rspec'
 require 'cocina/rspec'
-require 'zip'
 include LyberCore::Rspec # rubocop:disable Style/MixinUsage
 
 # Redirect log output to log/test.log instead of stdout
 Sidekiq.default_configuration.logger = Logger.new('log/test.log')
-
-def read_fixture(fname)
-  File.read(File.join(fixture_dir, fname))
-end
 
 def fixture_dir
   @fixture_dir ||= File.join(File.dirname(__FILE__), 'fixtures')
